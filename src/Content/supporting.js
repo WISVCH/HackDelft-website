8d83a2f4cba42bef706654da550c80b0a4a1561a
Paul Gerarts <paul@gerarts.nl>
2018-04-18 12:03:48 +0200

    Add Red Bull to site

--- src/Content/supporting.js
+++ src/Content/supporting.js
@@ -1,8 +1,8 @@
 const supporting = [
   {
-    asset: 'assets/mlh.png',
-    title: 'MLH',
-    href: 'https://mlh.io'
+    asset: "assets/mlh.png",
+    title: "MLH",
+    href: "https://mlh.io"
   },
   /* TODO: Publish these once public */
   // {
@@ -10,15 +10,15 @@ const supporting = [
   //   title: "Picnic",
   //   href: "https://www.picnic.nl"
   // },
-  // {
-  //   asset: "assets/rb.png",
-  //   title: "Red Bull",
-  //   href: "https://www.redbull.com/nl-nl/"
-  // },
   {
-    asset: 'assets/tudSC.jpg',
-    title: 'Sports and Culture - TU Delft',
-    href: 'https://www.tudelft.nl/sc/'
+    asset: "assets/rb.png",
+    title: "Red Bull",
+    href: "https://www.redbull.com/nl-nl/"
+  },
+  {
+    asset: "assets/tudSC.jpg",
+    title: "Sports and Culture - TU Delft",
+    href: "https://www.tudelft.nl/sc/"
   }
-];
-export default supporting;
+]
+export default supporting
